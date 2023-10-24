3a01fadc6f5b64e5a457ecc9d93d9eccd4ead659
Phu <phthienphu1210@gmail.com>
2023-10-24 23:05:57 +0700

    new file:   .vscode/settings.json 	modified:   Lab2/Config/__init__.py 	new file:   Lab2/Config/__init__.pyc 	new file:   Lab2/DataLoader/__init__.py 	new file:   Lab2/Metric/__init__.py 	new file:   Lab2/Model/__init__.py 	modified:   Lab2/SGD_main.py

--- Lab2/Config/__init__.py
+++ Lab2/Config/__init__.py
@@ -1,14 +1,9 @@
-config = {
-    "test_image_path": "archive/t10k-images-idx3-ubyte",
-    "test_label_path": "archive/t10k-labels-idx1-ubyte",
-    "train_image_path": "archive/train-images-idx3-ubyte",
-    "train_label_path": "archive/train-labels-idx1-ubyte",
-    "save_path": "./save",
-    "train_batch": 256,
-    "test_batch": 1000,
-    "n_epochs": 40,
-    "learning_rate": 0.01,
-    "momentum": 0.5,
-    "log_interval": 10,
-    "random_seed": 42
-}
+test_image_path = "archive/t10k-images-idx3-ubyte"
+test_label_path = "archive/t10k-labels-idx1-ubyte"
+train_image_path = "archive/train-images-idx3-ubyte"
+train_label_path = "archive/train-labels-idx1-ubyte"
+train_batch = 128
+test_batch = 1000
+n_epochs = 20
+learning_rate = 0.01
+momentum = 0.5
--- Lab2/SGD_main.py
+++ Lab2/SGD_main.py
@@ -1,10 +1,13 @@
-from Config import config
+from Config import train_image_path, train_label_path, test_image_path, test_label_path, train_batch, test_batch, n_epochs, learning_rate, momentum
 
-print(config["test_image_path"])
+# Path: Lab2/SGD_main.py
 
-# check if config path is correct
-import os
-print(os.path.isfile(config["test_image_path"]))
-
-# check current working directory
-print(os.getcwd())
+print(train_image_path)
+print(train_label_path)
+print(test_image_path)
+print(test_label_path)
+print(train_batch)
+print(test_batch)
+print(n_epochs)
+print(learning_rate)
+print(momentum)
