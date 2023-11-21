5706b9239b08b2e484d0244fb36985274fd403e9
Phu <phthienphu1210@gmail.com>
2023-11-21 21:52:49 +0700

    Create Inception block and GoogLeNet model

---
+++ Lab3/model/GoogLeNet.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class Inception(nn.Module):
+    # `c1`--`c4` are the number of output channels for each path
+    def __init__(self, in_channels, c1, c2, c3, c4, **kwargs):
+        super(Inception, self).__init__(**kwargs)
+        # Path 1 is a single 1 x 1 convolutional layer
+        self.p1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
+        # Path 2 is a 1 x 1 convolutional layer followed by a 3 x 3
+        # convolutional layer
+        self.p2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
+        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
+        # Path 3 is a 1 x 1 convolutional layer followed by a 5 x 5
+        # convolutional layer
+        self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
+        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+        # Path 4 is a 3 x 3 maximum pooling layer followed by a 1 x 1
+        # convolutional layer
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
+
+    def forward(self, x):
+        p1 = F.relu(self.p1_1(x))
+        p2 = F.relu(self.p2_2(F.relu(self.p2_1(x))))
+        p3 = F.relu(self.p3_2(F.relu(self.p3_1(x))))
+        p4 = F.relu(self.p4_2(self.p4_1(x)))
+        # Concatenate the outputs on the channel dimension
+        return torch.cat((p1, p2, p3, p4), dim=1)
+
+
+class googleNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            # nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3), #use 3 channels for rgb
+            # use 1 channel for grayscale
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=1),
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+        self.b1 = nn.Sequential(
+            Inception(192, 64, (96, 128), (16, 32), 32),
+            Inception(256, 128, (128, 192), (32, 96), 64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+        self.b2 = nn.Sequential(
+            Inception(480, 192, (96, 208), (16, 48), 64),
+            Inception(512, 160, (112, 224), (24, 64), 64),
+            Inception(512, 128, (128, 256), (24, 64), 64),
+            Inception(512, 112, (144, 288), (32, 64), 64),
+            Inception(528, 256, (160, 320), (32, 128), 128),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+        self.b3 = nn.Sequential(
+            Inception(832, 256, (160, 320), (32, 128), 128),
+            Inception(832, 384, (192, 384), (48, 128), 128),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(1024, 10),
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.b1(x)
+        x = self.b2(x)
+        x = self.b3(x)
+        x = self.fc(x)
+        return x
--- Lab3/model/__init__.py
+++ Lab3/model/__init__.py
@@ -1,1 +1,2 @@
 from .LeNet import LeNet
+from .GoogLeNet import googleNet
